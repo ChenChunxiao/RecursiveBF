953a6b15644cacab2272b0438443a57162848a60
Ming <a@ufoym.com>
2016-07-28 14:48:39 +0800

    Cleaner but buggy core

--- example/example.cpp
+++ example/example.cpp
@@ -1,9 +1,21 @@
 #include "../include/rbf.hpp"
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image.hpp"
+#include "stb_image_write.hpp"
 #include <stdio.h>
 #include <time.h>
 #define LEN_MAX	256
 
-int example_filtering(int argc, char*argv[]);
+
+class Timer {
+private:
+	unsigned long begTime;
+public:
+	void start() { begTime = clock(); }
+	float elapsedTime() { return float((unsigned long)clock() - begTime) / CLOCKS_PER_SEC; }
+};
+
 int main(int argc, char*argv[])
 {
 	if (argc != 5)
@@ -17,255 +29,26 @@ int main(int argc, char*argv[])
 	}
 	else
 	{
-		return example_filtering(argc, argv);
+		const char * filename_out = argv[1];
+		const char * filename_in = argv[2];
+		double sigma_spatial = atof(argv[3]);
+		double sigma_range = atof(argv[4]);
+
+		int width, height, channel;
+		unsigned char * img = stbi_load(filename_in, &width, &height, &channel, 0);
+
+		double * output = new double[width * height * channel];
+
+		Timer timer;
+		timer.start();
+		recursive_bf(img, output, sigma_spatial, sigma_range, width, height, channel);
+		printf("Elapsed time = %2.5f secs", timer.elapsedTime());
+
+		for (int i = 0; i < width * height * channel; ++i)
+			img[i] = (unsigned char)output[i];
+		stbi_write_bmp(filename_out, width, height, channel, img);
+
+		stbi_image_free(img);
+		delete [] output;
 	}
-}
-
-template <typename T>
-inline T *** qx_alloc_3(int n, int r, int c, int padding = 10)
-{
-	int rc = r*c;
-	int i, j;
-	T *a = (T*)malloc(sizeof(T)*(n*rc + padding));
-	if (a == NULL) { 
-		printf("qx_allocu_3() fail, Memory is too huge, fail.\n"); 
-		getchar(); 
-		exit(0); 
-	}
-	T **p = (T**)malloc(sizeof(T*)*n*r);
-	T ***pp = (T***)malloc(sizeof(T**)*n);
-	for (i = 0; i<n; i++)
-		for (j = 0; j<r; j++)
-			p[i*r + j] = &a[i*rc + j*c];
-	for (i = 0; i<n; i++)
-		pp[i] = &p[i*r];
-	return pp;
-}
-template <typename T>
-inline T ** qx_alloc(int r, int c, int padding = 10)
-{
-	T *a = (T*)malloc(sizeof(T)*(r*c + padding));
-	if (a == NULL) { 
-		printf("qx_allocd() fail, Memory is too huge, fail.\n"); 
-		getchar(); 
-		exit(0); 
-	}
-	T **p = (T**)malloc(sizeof(T*)*r);
-	for (int i = 0; i<r; i++) p[i] = &a[i*c];
-	return p;
-}
-
-template <typename T>
-inline void qx_free_3(T ***p)
-{
-	if (p != NULL)
-	{
-		free(p[0][0]);
-		free(p[0]);
-		free(p);
-		p = NULL;
-	}
-}
-template <typename T>
-inline void qx_free(T **p)
-{
-	if (p != NULL)
-	{
-		free(p[0]);
-		free(p);
-		p = NULL;
-	}
-}
-
-void qx_image_size(char* file_name, int &h, int &w, int *nr_channel = NULL)
-{
-	FILE * file_in; int nrc;
-	char line[LEN_MAX];
-	int	i;
-	unsigned char *image = NULL;
-	fopen_s(&file_in, file_name, "rb");
-	//fopen_s(&file_in,file_name,"rb");
-	if (!file_in)
-	{
-		printf("Please check input file_name: %s\n", file_name);
-		getchar();
-		exit(0);
-	}
-	if (fgetc(file_in) == 'P')
-		//fscanf_s(file_in,"%d\n",&i);
-		fscanf_s(file_in, "%d\n", &i);
-	else
-	{
-		printf("Bad	header in ppm file.\n");
-		getchar();
-		exit(1);
-	}
-	while (fgets(line, LEN_MAX, file_in) != NULL)
-	{
-		if (line[0] == '#') continue;
-		else
-		{
-			//sscanf_s(line, "%d %d\n",	&w,	&h);
-			sscanf_s(line, "%d %d\n", &w, &h);
-			if (i == 9 && nr_channel != NULL)
-			{
-				fscanf_s(file_in, "%d\n", &nrc);
-				(*nr_channel) = nrc;
-			}
-			break;
-		}
-	}
-}
-
-int qx_loadimage(char* filename, unsigned char *image, int h, int w, int *nr_channel = NULL)
-{
-	FILE * file_in; int nrc;
-	char line[LEN_MAX];
-	int	i; int imax, hc, wc;
-	unsigned char *image_ = image;
-	//fopen_s(&file_in,filename,"rb");
-	fopen_s(&file_in, filename, "rb");
-
-	if (!file_in)
-	{
-		printf("Please check input filename: %s\n", filename);
-		exit(0);
-	}
-	if (fgetc(file_in) == 'P')
-		fscanf_s(file_in, "%d\n", &i);
-	else
-	{
-		printf("Bad	header in ppm file.\n");
-		exit(1);
-	}
-	while (fgets(line, LEN_MAX, file_in) != NULL)
-	{
-		if (line[0] == '#') continue;
-		else
-		{
-			sscanf_s(line, "%d %d\n", &wc, &hc);
-			break;
-		}
-	}
-	char str_tmp[100];
-	switch (i)
-	{
-	case 5:
-		fgets(str_tmp, 100, file_in);
-		imax = atoi(str_tmp);
-		if (nr_channel != NULL) (*nr_channel) = 1;
-		memset(image, 0, sizeof(unsigned char)*h*w);
-		fread(image, sizeof(unsigned char), h*w, file_in);
-		break;
-	case 6:
-		fgets(str_tmp, 100, file_in);
-		imax = atoi(str_tmp);
-		if (nr_channel != NULL) (*nr_channel) = 3;
-		memset(image, 0, sizeof(unsigned char)*h*w * 3);
-		fread(image, sizeof(unsigned char), h*w * 3, file_in);
-		break;
-	case 2:
-		fgets(str_tmp, 100, file_in);
-		imax = atoi(str_tmp);
-		for (int y = 0; y<h; y++) for (int x = 0; x<w; x++)
-		{
-			//if(fscanf_s(file_in,"%d",&imax)!=1){printf("error in reading file.\n");getchar();exit(0);}
-			fscanf_s(file_in, "%d", &imax);
-			*image_++ = imax;
-		}
-		break;
-	case 3:
-		fgets(str_tmp, 100, file_in);
-		imax = atoi(str_tmp);
-		int cr, cg, cb;
-		for (int y = 0; y<h; y++) for (int x = 0; x<w; x++)
-		{
-			//if(fscanf_s(file_in,"%d%d%d",&cr,&cg,&cb)!=3){printf("error in reading file.\n");getchar();exit(0);}
-			fscanf_s(file_in, "%d%d%d", &cr, &cg, &cb);
-			*image_++ = cr; *image_++ = cg; *image_++ = cb;
-		}
-		break;
-	case 9:
-		fgets(str_tmp, 100, file_in);
-		nrc = atoi(str_tmp);
-		fscanf_s(file_in, "%d\n", &nrc);
-		if (nr_channel != NULL) (*nr_channel) = nrc;
-		fgets(str_tmp, 100, file_in);
-		imax = atoi(str_tmp);
-		fread(image, sizeof(unsigned char), h*w*nrc, file_in);
-		break;
-	default:
-		printf("Can not open image [%s]!!\n", filename);
-		break;
-	}
-	fclose(file_in);
-	return (0);
-}
-
-void qx_saveimage(char* filename, unsigned char *image, int h, int w, int channel)
-{
-	FILE* file_out; unsigned char maxx = 255;
-	//fopen_s(&file_out,filename,"wb");
-	fopen_s(&file_out, filename, "wb");
-
-	if (channel == 1) fprintf(file_out, "P5\n%d %d\n%d\n", w, h, maxx);
-	//else if(channel==3) fprintf(file_out,"P6\n%d %d\n%d\n",w,h,maxx);
-	else if (channel == 3) fprintf(file_out, "P6\n%d %d\n%d\n", w, h, maxx);
-	else fprintf(file_out, "P9\n%d %d\n%d\n%d\n", w, h, channel, maxx);
-	fwrite(image, sizeof(unsigned char), h*w*channel, file_out);
-
-	fclose(file_out);
-}
-
-class Timer {
-private:
-	unsigned long begTime;
-public:
-	void start() { begTime = clock(); }
-	float elapsedTime() { return float((unsigned long)clock() - begTime) / CLOCKS_PER_SEC; }
-};
-
-
-int example_filtering(int argc, char*argv[])
-{
-	char*filename_out = argv[1];
-	char*filename_in = argv[2];
-	double sigma_spatial = atof(argv[3]);
-	double sigma_range = atof(argv[4]);
-
-	int h, w;//height and width of the input image
-	qx_image_size(filename_in, h, w);//obtain the height and width of the input image
-	unsigned char ***texture = qx_alloc_3<unsigned char>(h, w, 3);//allocate memory
-	double ***image = qx_alloc_3<double>(h, w, 3);
-	double ***image_filtered = qx_alloc_3<double>(h, w, 3);
-	double ***temp = qx_alloc_3<double>(h, w, 3);
-	double ***temp_2 = qx_alloc_3<double>(2, w, 3);
-
-	qx_loadimage(filename_in, texture[0][0], h, w);//read an color image
-	for (int y = 0; y<h; y++) for (int x = 0; x<w; x++) for (int c = 0; c<3; c++) image[y][x][c] = texture[y][x][c];//initialize the original color image
-
-	Timer timer;
-	timer.start();
-
-	double**temp_factor = qx_alloc<double>(h * 2 + 2, w);
-	qx_recursive_bilateral_filter(
-		image_filtered, image, texture,
-		sigma_spatial, sigma_range, h, w, temp, temp_2,
-		temp_factor, &(temp_factor[h]), &(temp_factor[h + h]));
-	qx_free(temp_factor);
-	temp_factor = NULL;
-	printf("Elapsed time: %2.5f", timer.elapsedTime());
-
-	for (int y = 0; y<h; y++) 
-		for (int x = 0; x<w; x++) 
-			for (int c = 0; c<3; c++) 
-				texture[y][x][c] = (unsigned char)image_filtered[y][x][c];
-	qx_saveimage(filename_out, texture[0][0], h, w, 3);
-
-	qx_free_3(texture);
-	qx_free_3(image);
-	qx_free_3(image_filtered);
-	qx_free_3(temp);
-	qx_free_3(temp_2);
-	return(0);
 }
--- include/rbf.hpp
+++ include/rbf.hpp
@@ -13,97 +13,129 @@ European Conference on Computer Vision (ECCV) 2012, 399-413.
 //Large sigma_spatial/sigma_range parameter may results in visible artifact which can be removed by 
 //an additional filter with small sigma_spatial/sigma_range parameter.
 
-void qx_recursive_bilateral_filter(double***out, double***in, unsigned char***texture, double sigma_spatial, double sigma_range, int h, int w, double***temp, double***temp_2w, double**factor, double**temp_factor, double**temp_factor_2w)
+void recursive_bf(
+	unsigned char * texture, double * out,
+	double sigma_spatial, double sigma_range, 
+	int width, int height, int channel)
 {
-	double range_table[QX_DEF_CHAR_MAX + 1];//compute a lookup table
-	double inv_sigma_range;
-	inv_sigma_range = 1.0 / (sigma_range*QX_DEF_CHAR_MAX);
-	for (int i = 0; i <= QX_DEF_CHAR_MAX; i++) range_table[i] = exp(-i*inv_sigma_range);
-	double alpha = exp(-sqrt(2.0) / (sigma_spatial*w));//filter kernel size
-	double***in_ = in;
-	unsigned char tpr, tpg, tpb, tcr, tcg, tcb;
-	double ypr, ypg, ypb, ycr, ycg, ycb;
-	double fp, fc;//factor
-	double inv_alpha_ = 1 - alpha;
-	for (int y = 0; y<h; y++)/*horizontal filtering*/
-	{
-		double*temp_x = temp[y][0];
-		double*in_x = in_[y][0];
-		unsigned char*texture_x = texture[y][0];
-		*temp_x++ = ypr = *in_x++; *temp_x++ = ypg = *in_x++; *temp_x++ = ypb = *in_x++;
-		tpr = *texture_x++; tpg = *texture_x++; tpb = *texture_x++;
+	double * in = new double[width * height * channel];
+	for (int i = 0; i < width * height * channel; ++i)
+		in[i] = texture[i];
 
-		double*temp_factor_x = temp_factor[y];//factor
+	//double * image = new double(width * height * channel);
+	//double * image_filtered = new double(width * height * channel);
+	double * temp = new double[width * height * channel];
+	//double * temp_2 = new double[width * 2 * channel];
+	double * temp_factor = new double[width * (height * 2 + 2)];
+
+	//compute a lookup table
+	double range_table[QX_DEF_CHAR_MAX + 1];
+	double inv_sigma_range = 1.0 / (sigma_range * QX_DEF_CHAR_MAX);
+	for (int i = 0; i <= QX_DEF_CHAR_MAX; i++) 
+		range_table[i] = exp(-i * inv_sigma_range);
+
+	double alpha = exp(-sqrt(2.0) / (sigma_spatial * width));
+	double ypr, ypg, ypb, ycr, ycg, ycb;
+	double fp, fc;
+	double inv_alpha_ = 1 - alpha;
+	for (int y = 0; y < height; y++)
+	{
+		double * temp_x = &temp[y * width * channel];
+		double * in_x = &in[y * width * channel];
+		unsigned char * texture_x = &texture[y * width * channel];
+		*temp_x++ = ypr = *in_x++; 
+		*temp_x++ = ypg = *in_x++; 
+		*temp_x++ = ypb = *in_x++;
+		unsigned char tpr = *texture_x++; 
+		unsigned char tpg = *texture_x++;
+		unsigned char tpb = *texture_x++;
+
+		double * temp_factor_x = &temp_factor[y * width];
 		*temp_factor_x++ = fp = 1;
-		for (int x = 1; x<w; x++) //from left to right
+		for (int x = 1; x < width; x++) // from left to right
 		{
-			tcr = *texture_x++; tcg = *texture_x++; tcb = *texture_x++;
+			unsigned char tcr = *texture_x++; 
+			unsigned char tcg = *texture_x++; 
+			unsigned char tcb = *texture_x++;
 			unsigned char dr = abs(tcr - tpr);
 			unsigned char dg = abs(tcg - tpg);
 			unsigned char db = abs(tcb - tpb);
 			int range_dist = (((dr << 1) + dg + db) >> 2);
 			double weight = range_table[range_dist];
 			double alpha_ = weight*alpha;
-			*temp_x++ = ycr = inv_alpha_*(*in_x++) + alpha_*ypr; *temp_x++ = ycg = inv_alpha_*(*in_x++) + alpha_*ypg; *temp_x++ = ycb = inv_alpha_*(*in_x++) + alpha_*ypb;//update temp buffer
+			*temp_x++ = ycr = inv_alpha_*(*in_x++) + alpha_*ypr; 
+			*temp_x++ = ycg = inv_alpha_*(*in_x++) + alpha_*ypg; 
+			*temp_x++ = ycb = inv_alpha_*(*in_x++) + alpha_*ypb;
 			tpr = tcr; tpg = tcg; tpb = tcb;
 			ypr = ycr; ypg = ycg; ypb = ycb;
-			*temp_factor_x++ = fc = inv_alpha_ + alpha_*fp;//factor
+			*temp_factor_x++ = fc = inv_alpha_ + alpha_*fp;
 			fp = fc;
 		}
-		int w1 = w - 1;
 		*--temp_x; *temp_x = 0.5*((*temp_x) + (*--in_x));
 		*--temp_x; *temp_x = 0.5*((*temp_x) + (*--in_x));
 		*--temp_x; *temp_x = 0.5*((*temp_x) + (*--in_x));
-		tpr = *--texture_x; tpg = *--texture_x; tpb = *--texture_x;
+		tpr = *--texture_x; 
+		tpg = *--texture_x; 
+		tpb = *--texture_x;
 		ypr = *in_x; ypg = *in_x; ypb = *in_x;
 
-		*--temp_factor_x; *temp_factor_x = 0.5*((*temp_factor_x) + 1);//factor
+		*--temp_factor_x; *temp_factor_x = 0.5*((*temp_factor_x) + 1);
 		fp = 1;
 
-		for (int x = w - 2; x >= 0; x--) //from right to left
+		for (int x = width - 2; x >= 0; x--) // from right to left
 		{
-			tcr = *--texture_x; tcg = *--texture_x; tcb = *--texture_x;
+			unsigned char tcr = *--texture_x; 
+			unsigned char tcg = *--texture_x; 
+			unsigned char tcb = *--texture_x;
 			unsigned char dr = abs(tcr - tpr);
 			unsigned char dg = abs(tcg - tpg);
 			unsigned char db = abs(tcb - tpb);
 			int range_dist = (((dr << 1) + dg + db) >> 2);
 			double weight = range_table[range_dist];
-			double alpha_ = weight*alpha;
+			double alpha_ = weight * alpha;
 
-			ycr = inv_alpha_*(*--in_x) + alpha_*ypr; ycg = inv_alpha_*(*--in_x) + alpha_*ypg; ycb = inv_alpha_*(*--in_x) + alpha_*ypb;
+			ycr = inv_alpha_ * (*--in_x) + alpha_ * ypr; 
+			ycg = inv_alpha_ * (*--in_x) + alpha_ * ypg; 
+			ycb = inv_alpha_ * (*--in_x) + alpha_ * ypb;
 			*--temp_x; *temp_x = 0.5*((*temp_x) + ycr);
 			*--temp_x; *temp_x = 0.5*((*temp_x) + ycg);
 			*--temp_x; *temp_x = 0.5*((*temp_x) + ycb);
 			tpr = tcr; tpg = tcg; tpb = tcb;
 			ypr = ycr; ypg = ycg; ypb = ycb;
 
-			fc = inv_alpha_ + alpha_*fp;//factor
-			*--temp_factor_x; *temp_factor_x = 0.5*((*temp_factor_x) + fc);
+			fc = inv_alpha_ + alpha_*fp;
+			*--temp_factor_x; 
+			*temp_factor_x = 0.5*((*temp_factor_x) + fc);
 			fp = fc;
 		}
 	}
-	alpha = exp(-sqrt(2.0) / (sigma_spatial*h));//filter kernel size
+	alpha = exp(-sqrt(2.0) / (sigma_spatial * height));
 	inv_alpha_ = 1 - alpha;
-	in_ = temp;/*vertical filtering*/
-	double*ycy, *ypy, *xcy;
-	unsigned char*tcy, *tpy;
-	memcpy(out[0][0], temp[0][0], sizeof(double)*w * 3);
+	in = temp;
+	double * ycy, * ypy, * xcy;
+	unsigned char * tcy, * tpy;
+	memcpy(out, temp, sizeof(double)* width * channel);
 
-	double**in_factor = temp_factor;//factor
+	double * in_factor = temp_factor;
 	double*ycf, *ypf, *xcf;
-	memcpy(factor[0], in_factor[0], sizeof(double)*w);
-	for (int y = 1; y<h; y++)
+	double * factor_a = new double[width * height];
+	double * factor_b = new double[width];
+	double * factor_c = new double[width];
+	double * factor_cb = new double[width * channel];
+	double * factor_cc = new double[width * channel];
+	memcpy(factor_a, in_factor, sizeof(double) * width);
+	for (int y = 1; y < height; y++)
 	{
-		tpy = texture[y - 1][0];
-		tcy = texture[y][0];
-		xcy = in_[y][0];
-		ypy = out[y - 1][0];
-		ycy = out[y][0];
+		tpy = &texture[(y - 1) * width * channel];
+		tcy = &texture[y * width * channel];
+		xcy = &in[y * width * channel];
+		ypy = &out[(y - 1) * width * channel];
+		ycy = &out[y * width * channel];
 
-		xcf = &in_factor[y][0];//factor
-		ypf = &factor[y - 1][0];
-		ycf = &factor[y][0];
-		for (int x = 0; x<w; x++)
+		xcf = &in_factor[y * width];
+		ypf = &factor_a[(y - 1) * width];
+		ycf = &factor_a[y * width];
+		for (int x = 0; x < width; x++)
 		{
 			unsigned char dr = abs((*tcy++) - (*tpy++));
 			unsigned char dg = abs((*tcy++) - (*tpy++));
@@ -111,35 +143,43 @@ void qx_recursive_bilateral_filter(double***out, double***in, unsigned char***te
 			int range_dist = (((dr << 1) + dg + db) >> 2);
 			double weight = range_table[range_dist];
 			double alpha_ = weight*alpha;
-			for (int c = 0; c<3; c++) *ycy++ = inv_alpha_*(*xcy++) + alpha_*(*ypy++);
+			for (int c = 0; c < channel; c++) 
+				*ycy++ = inv_alpha_*(*xcy++) + alpha_*(*ypy++);
 			*ycf++ = inv_alpha_*(*xcf++) + alpha_*(*ypf++);
 		}
 	}
-	int h1 = h - 1;
-	ycf = &temp_factor_2w[0][0];//factor
-	ypf = &temp_factor_2w[1][0];
-	memcpy(ypf, in_factor[h1], sizeof(double)*w);
-	for (int x = 0; x<w; x++) factor[h1][x] = 0.5*(factor[h1][x] + ypf[x]);
+	int h1 = height - 1;
+	ycf = factor_b;
+	ypf = factor_c;
+	memcpy(ypf, &in_factor[h1 * width], sizeof(double) * width);
+	for (int x = 0; x < width; x++) 
+		factor_a[h1 * width + x] = 0.5*(factor_a[h1 * width + x] + ypf[x]);
 
-	ycy = temp_2w[0][0];
-	ypy = temp_2w[1][0];
-	memcpy(ypy, in_[h1][0], sizeof(double)*w * 3);
-	int k = 0; for (int x = 0; x<w; x++) for (int c = 0; c<3; c++) out[h1][x][c] = 0.5*(out[h1][x][c] + ypy[k++]) / factor[h1][x];
+	ycy = factor_cb;
+	ypy = factor_cc;
+	memcpy(ypy, &in[h1 * width], sizeof(double) * width * channel);
+	int k = 0; 
+	for (int x = 0; x < width; x++) {
+		for (int c = 0; c < channel; c++) {
+			int idx = (h1 * width + x) * channel + c;
+			out[idx] = 0.5*(out[idx] + ypy[k++]) / factor_a[h1 * width + x];
+		}
+	}
 
 	for (int y = h1 - 1; y >= 0; y--)
 	{
-		tpy = texture[y + 1][0];
-		tcy = texture[y][0];
-		xcy = in_[y][0];
+		tpy = &texture[(y + 1) * width * channel];
+		tcy = &texture[y * width * channel];
+		xcy = &in[y * width * channel];
 		double*ycy_ = ycy;
 		double*ypy_ = ypy;
-		double*out_ = out[y][0];
+		double*out_ = &out[y * width * channel];
 
-		xcf = &in_factor[y][0];//factor
+		xcf = &in_factor[y * width];
 		double*ycf_ = ycf;
 		double*ypf_ = ypf;
-		double*factor_ = &factor[y][0];
-		for (int x = 0; x<w; x++)
+		double*factor_ = &factor_a[y * width];
+		for (int x = 0; x < width; x++)
 		{
 			unsigned char dr = abs((*tcy++) - (*tpy++));
 			unsigned char dg = abs((*tcy++) - (*tpy++));
@@ -148,25 +188,20 @@ void qx_recursive_bilateral_filter(double***out, double***in, unsigned char***te
 			double weight = range_table[range_dist];
 			double alpha_ = weight*alpha;
 
-			double fcc = inv_alpha_*(*xcf++) + alpha_*(*ypf_++);//factor
+			double fcc = inv_alpha_*(*xcf++) + alpha_*(*ypf_++);
 			*ycf_++ = fcc;
 			*factor_ = 0.5*(*factor_ + fcc);
 
-			for (int c = 0; c<3; c++)
+			for (int c = 0; c < channel; c++)
 			{
-				//ycy[x][c]=inv_alpha_*xcy[x][c]+alpha_*ypy[x][c];
-				//out[y][x][c]=0.5*(out[y][x][c]+ycy[x][c]);
 				double ycc = inv_alpha_*(*xcy++) + alpha_*(*ypy_++);
 				*ycy_++ = ycc;
-				//*out_=0.5*(*out_+ycc); 
 				*out_ = 0.5*(*out_ + ycc) / (*factor_);
 				*out_++;
 			}
 			*factor_++;
 		}
-		memcpy(ypy, ycy, sizeof(double)*w * 3);
-		memcpy(ypf, ycf, sizeof(double)*w);//factor
-
-		//for(int x=0;x<w;x++) for(int c=0;c<3;c++) out[y][x][c]/=factor[y][x];
+		memcpy(ypy, ycy, sizeof(double) * width * channel);
+		memcpy(ypf, ycf, sizeof(double) * width);
 	}
 }
